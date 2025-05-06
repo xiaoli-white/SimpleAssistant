969fecada2099a8b1d66a4cb5610986c04809514
XiaoLi <maxljh0216@yeah.net>
2025-05-06 22:04:27 +0800

    feat(builtin_mcp_tools): Add tools about file i/o.

--- builtin_mcp_tools.py
+++ builtin_mcp_tools.py
@@ -1,6 +1,7 @@
+import os.path
 import subprocess
 from datetime import datetime
-from typing import Any
+from typing import Any, Optional, Union
 
 from mcp.server.fastmcp import FastMCP
 
@@ -13,10 +14,56 @@ def execute_command(command: list[str]) -> dict[str, Any]:
     result: subprocess.CompletedProcess = subprocess.run(command, capture_output=True, text=True)
     return {"returnCode": result.returncode, "stdout": result.stdout, "stderr": result.stderr}
 
+
 @mcp.tool()
 def current_time() -> str:
     """Get the current time."""
     return "The current time is " + str(datetime.now())
+
+
+@mcp.tool()
+def create_file(filepath: str) -> Optional[Exception]:
+    """Create a file."""
+    if os.path.exists(filepath):
+        return None
+    try:
+        with open(filepath, "w") as f:
+            pass
+        return None
+    except Exception as e:
+        return e
+
+
+@mcp.tool()
+def read_file(filepath: str, encoding: str = "utf-8", isBinary: bool = False) -> Union[str, Exception]:
+    """Read a file."""
+    try:
+        with open(filepath, "rb" if isBinary else "r", encoding=encoding) as f:
+            return f.read()
+    except Exception as e:
+        return e
+
+
+@mcp.tool()
+def write_file(filepath: str, content: str, encoding: str = "utf-8", isBinary: bool = False) -> Optional[Exception]:
+    """Write a file."""
+    try:
+        with open(filepath, "wb" if isBinary else "w", encoding=encoding) as f:
+            f.write(content)
+        return None
+    except Exception as e:
+        return e
+
+
+@mcp.tool()
+def delete_file(filepath: str) -> Optional[Exception]:
+    """Delete a file."""
+    try:
+        os.remove(filepath)
+        return None
+    except Exception as e:
+        return e
+
 
 if __name__ == '__main__':
     mcp.run(transport="sse")
